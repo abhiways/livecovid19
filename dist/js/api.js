40a5b0dfeaa6038bf4f66d793f91b56c9e80f61a
Administrator <mobipride.com@gmail.com>
2020-04-19 17:15:56 +0530

    API Improved for speed

--- dist/js/api.js
+++ dist/js/api.js
@@ -25,17 +25,7 @@ var messages = document.getElementById("messages");
     RecoveredToday.innerText = data.RecoveredToday;
     DeceasedToday.innerText = data.DeceasedToday;
     /* State Updates */
-        var currdate = (new Date()).toString().split(' ').splice(1,3).join('');
-        var size = Object.size(data.jsonData[currdate]);
-        var date = new Date();
-        date.setDate(date.getDate()-1);
-        var day1mo = new Date();
-        day1mo.setDate(date.getDate()-30);
-        if(size == 0) {
-          currdate = (date).toString().split(' ').splice(1,3).join('');
-         }
-        var yesterday = (date).toString().split(' ').splice(1,3).join('');
-        statesDataUpdate(data.jsonData, currdate, yesterday, 2);
+        statesDataUpdate(data.jsonData, 2);
     /* State Updates Ends */
 });
 
@@ -50,13 +40,13 @@ Object.size = function(obj) {
   return size;
 };
 
-function statesDataUpdate(data, currdate, yesterday, upType) {
+function statesDataUpdate(data, upType) {
    /* Add States Data */
-   for (var key in data[currdate].state_wise) {
+   for (var key in data.data.state_wise) {
     // skip loop if the property is from prototype
-    if (!data[currdate].state_wise.hasOwnProperty(key)) continue;
+    if (!data.data.state_wise.hasOwnProperty(key)) continue;
 
-    var obj = data[currdate].state_wise[key];
+    var obj = data.data.state_wise[key];
     for (var prop in obj) {
         // skip loop if the property is from prototype
         if (!obj.hasOwnProperty(prop)) continue;
@@ -64,40 +54,32 @@ function statesDataUpdate(data, currdate, yesterday, upType) {
         if(prop == "state") { var state = obj[prop]; }
         if(prop == "active") { var active = obj[prop]; }
         var statecode = obj.statecode;
-        if(prop == "confirmed") { var confirmed = obj[prop]; 
-          var confirmedyest = data[yesterday].state_wise[key].confirmed; 
-          if(confirmed != confirmedyest) {
-            var newconfirmed = confirmed - confirmedyest;
+        if(prop == "confirmed") { var confirmed = obj[prop];
+        if(prop == "deltaconfirmed") { var newconfirmed = obj[prop]; }
+          var newconfirmed = obj.deltaconfirmed; 
             if(upType == 1) {
             confirmed = "<span id='confirmed-"+statecode+"'>"+confirmed+"</span> <i class='fa fa-caret-up color-red'></i>  <span id='newconfirmed-"+statecode+"' class='rowsmalltxt'> "+newconfirmed+"</span>";
             } else {
               $("#confirmed-"+statecode+"").html(""+confirmed+"");
               $("#newconfirmed-"+statecode+"").html(""+newconfirmed+"");
             }
-          }
         }
         if(prop == "deaths") { var deaths = obj[prop];
-          var deathsyest = data[yesterday].state_wise[key].deaths; 
-          if(deaths != deathsyest) {
-            var newdeaths = deaths - deathsyest;
+          var newdeaths = obj.deltadeaths;
             if(upType == 1) {
             deaths = "<span id='deaths-"+statecode+"'>"+deaths+" <i class='fa fa-caret-up color-red'></i>  <span id='newdeaths-"+statecode+"' class='rowsmalltxt'> "+newdeaths+"</span>";
             } else {
               $("#deaths-"+statecode+"").html(""+deaths+"");
               $("#newdeaths-"+statecode+"").html(""+newdeaths+"");
             }
-          }
         }
         if(prop == "recovered") { var recovered = obj[prop]; 
-          var recoveredyest = data[yesterday].state_wise[key].recovered; 
-          if(recovered != recoveredyest) {
-            var newrecovered = recovered - recoveredyest;
+          var newrecovered = obj.deltarecovered;
             if(upType == 1) {
             recovered = "<span id='recovered-"+statecode+"'>"+recovered+" <i class='fa fa-caret-up color-green'></i>  <span id='newrecovered-"+statecode+"' class='rowsmalltxt'> "+newrecovered+"</span>";
             } else {
               $("#recovered-"+statecode+"").html(""+recovered+"");
               $("#newrecovered-"+statecode+"").html(""+newrecovered+"");
-            }
           }
         }
         if(prop == "lastupdatedtime") { 
@@ -166,63 +148,68 @@ if ($('.table').length > 0) {
     })
     .then(json => {
       json.map(data => {
-        var currdate = (new Date()).toString().split(' ').splice(1,3).join('');
-        var size = Object.size(data[currdate]);
-        var date = new Date();
-        date.setDate(date.getDate()-1);
-        var day1mo = new Date();
-        day1mo.setDate(date.getDate()-30);
-        if(size == 0) {
-          currdate = (date).toString().split(' ').splice(1,3).join('');
-         }
-        var yesterday = (date).toString().split(' ').splice(1,3).join('');
-        Confirmed.innerText = data[currdate].total_values.confirmed;
-        ConfirmedToday.innerText = data[currdate].total_values.deltaconfirmed;
-        Active.innerText = data[currdate].total_values.active;
-        //ActiveToday.innerText = data[currdate].total_values.deltaconfirmed;
-        Recovered.innerText = data[currdate].total_values.recovered;
-        RecoveredToday.innerText = data[currdate].total_values.deltarecovered;
-        Deceased.innerText = data[currdate].total_values.deaths;
-        DeceasedToday.innerText = data[currdate].total_values.deltadeaths;
-        var pointlength = data.daily.length;
-        statesDataUpdate(data, currdate, yesterday, 1);
-      nv.addGraph(function() {
-        var maxy = Math.ceil(data[yesterday].total_values.confirmed/1000)*1000;
-        var chart = nv.models.lineWithFocusChart().yDomain([0,maxy]).rightAlignYAxis(true)
-            .useInteractiveGuideline(true)
-            .margin({top: 0, bottom: 25, left: 0, right: 25})
-            //.showLegend(false)
-            .color([
-                '#6294c9', '#59bc79', '#eb4034'
-            ]);
-
-        chart.legend.margin({top: 3});
-
-        chart.yAxis
-            .showMaxMin(false)
-            .tickFormat(d3.format("s"));
-
-        chart.xAxis
-            .showMaxMin(false)
-            .tickFormat(function(d) { return d3.time.format('%b %d')(new Date(d)) });
-
-        chart.x2Axis
-            .showMaxMin(false)
-            .tickFormat(function(d) { return d3.time.format('%b %d')(new Date(d)) });
-
-       chart.brushExtent([new Date(day1mo.getTime()), new Date()])
-
-        var data1 = ApiData(['Confirmed', 'Recovered', 'Deceased'], pointlength, data.daily);
-        data1[0].area = true;
-        d3.select('#visits-chart svg')
-            .datum(data1)
-            .transition().duration(500)
-            .call(chart);
-
-        PjaxApp.onResize(chart.update);
-
-        return chart;
+        Confirmed.innerText = data.data.total_values.confirmed;
+        ConfirmedToday.innerText = data.data.total_values.deltaconfirmed;
+        Active.innerText = data.data.total_values.active;
+        //ActiveToday.innerText = data.data.total_values.deltaconfirmed;
+        Recovered.innerText = data.data.total_values.recovered;
+        RecoveredToday.innerText = data.data.total_values.deltarecovered;
+        Deceased.innerText = data.data.total_values.deaths;
+        DeceasedToday.innerText = data.data.total_values.deltadeaths;
+        statesDataUpdate(data, 1);
+      });
     });
+})();
+
+// fetching iniatial daily stats from the database
+(function() {
+  fetch("https://www.livecovid19.org/api/dailystats")
+    .then(data => {
+      return data.json();
+    })
+    .then(json => {
+      json.map(data => {
+        var pointlength = data.daily.length;
+        var day1mo = new Date();
+        day1mo.setDate(day1mo.getDate()-30);
+        nv.addGraph(function() {
+          var maxf = data.daily[Object.keys(data.daily)[Object.keys(data.daily).length-1]];
+          var maxy = Math.ceil(maxf.totalconfirmed/1000)*1000;
+          var chart = nv.models.lineWithFocusChart().yDomain([0,maxy]).rightAlignYAxis(true)
+              .useInteractiveGuideline(true)
+              .margin({top: 0, bottom: 25, left: 0, right: 25})
+              //.showLegend(false)
+              .color([
+                  '#6294c9', '#59bc79', '#eb4034'
+              ]);
+
+          chart.legend.margin({top: 3});
+
+          chart.yAxis
+              .showMaxMin(false)
+              .tickFormat(d3.format("s"));
+
+          chart.xAxis
+              .showMaxMin(false)
+              .tickFormat(function(d) { return d3.time.format('%b %d')(new Date(d)) });
+
+          chart.x2Axis
+              .showMaxMin(false)
+              .tickFormat(function(d) { return d3.time.format('%b %d')(new Date(d)) });
+
+        chart.brushExtent([new Date(day1mo.getTime()), new Date()])
+
+          var data1 = ApiData(['Confirmed', 'Recovered', 'Deceased'], pointlength, data.daily);
+          data1[0].area = true;
+          d3.select('#visits-chart svg')
+              .datum(data1)
+              .transition().duration(500)
+              .call(chart);
+
+          PjaxApp.onResize(chart.update);
+
+          return chart;
+        });
       });
     });
 })();
